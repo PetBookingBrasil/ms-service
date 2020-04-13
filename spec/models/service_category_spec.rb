a17873b7844eb0bcd3ef1b281835e86ccd80b38d
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-04-13 13:59:52 -0300

    unit tests for service category creation

--- spec/models/service_category_spec.rb
+++ spec/models/service_category_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe ServiceCategory, type: :model do
   describe "Validations of Service Category" do
     let!(:service_category_invalid){ build(:service_category_invalid) }
-    let!(:service_category){ create(:service_category) }
+    let!(:service_category){ build(:service_category) }
     
     describe "validations" do
       subject { service_category_invalid } 
@@ -21,6 +21,34 @@ RSpec.describe ServiceCategory, type: :model do
         expect(subject.valid?).to be_falsey
         expect(subject.errors).to have(4).items
       end
+    end
+
+    describe 'save operations' do
+      subject { service_category }
+
+      context "service category without root" do
+        it { is_expected.to be_valid }
+        it "should save root service category" do
+          expect(subject.save).to be_truthy
+        end
+      end
+
+      context "service category with root" do
+        before do
+          service_category.save
+        end
+
+        let(:service_category_children) { create(:service_category, parent: service_category) }
+
+        it "should be valid" do
+          expect(service_category_children).to be_valid
+        end
+
+        it "should have parent equal" do
+          expect(service_category_children.parent).to eql(service_category)
+        end
+      end
+      
     end
   end
 end
