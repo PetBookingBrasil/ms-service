7913af00bc2e3d6520f9bcebf88a49143907734e
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-04-13 15:34:49 -0300

    specs for creation with position and negative scenarios for service category

--- app/controllers/service_categories_controller.rb
+++ app/controllers/service_categories_controller.rb
@@ -13,8 +13,20 @@ class ServiceCategoriesController < ApplicationController
   end
 
   def create
+    service_category = ServiceCategory.new(service_category_params)
+    if service_category.save
+      render json: service_category, status: 201
+    else
+      render json: service_category.errors , status: 400
+    end    
   end
 
   def update
   end
+
+  protected
+
+    def service_category_params
+      params.require(:service_category).permit(:id, :uuid, :business_id, :name, :slug, :system_code)
+    end
 end
--- spec/controllers/service_categorie_request_spec.rb
+++ spec/controllers/service_categorie_request_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ServiceCategoriesController do
     @request.env['X-Signature'] = header_signature
   end
 
-  describe "GET index with tree of Service Categories" do
+  describe 'GET index with tree of Service Categories' do
     let(:body) { JSON.parse(response.body) }
     before do
       10.times do
@@ -17,18 +17,18 @@ RSpec.describe ServiceCategoriesController do
       get :index
     end
     
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
 
-    it "returns service categories with childrens" do
+    it 'returns service categories with childrens' do
       expect(body).to have(10).items
     end
   end
 
-  describe "Get Service Categories searching by business" do
+  describe 'Get Service Categories searching by business' do
     let(:body) { JSON.parse(response.body) }
-    context "when the business is informed" do
+    context 'when the business is informed' do
       before do
         create_list(:service_category, 10, business_id: 1)
         create_list(:service_category, 10, business_id: 2)
@@ -36,42 +36,73 @@ RSpec.describe ServiceCategoriesController do
         get :filter, params: {business_id: '1|3'}
       end
       
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
   
-      it "returns service categories by search business" do
+      it 'returns service categories by search business' do
         expect(body).to have(20).items
       end
     end
 
-    context "when the business is informed but not exists" do
+    context 'when the business is informed but not exists' do
       before do
         create_list(:service_category, 10, business_id: 1)
         get :filter, params: {business_id: '4'}
       end
       
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
   
-      it "returns zero service categories by search business" do
+      it 'returns zero service categories by search business' do
         expect(body).to have(0).items
       end
     end
     
-    context "when the business isnt informed" do
+    context 'when the business isnt informed' do
       before do
         create_list(:service_category, 10, business_id: 1)
         get :filter
       end
       
-      it "returns http success" do
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
       end
   
-      it "returns zero service categories by search business" do
+      it 'returns zero service categories by search business' do
         expect(body).to have(0).items
+      end
+    end
+  end
+
+  describe 'Endpoint to service category creation' do
+    let(:body) { JSON.parse(response.body) }
+    let!(:service_category) { build(:service_category, :with_parent) }
+
+    context 'save service category root' do
+      let(:valid_attributes) { attributes_for(:service_category) }
+
+      context 'with valid paramters' do
+        it 'creates a ServiceCategory' do
+          expect {
+            post :create, params: { service_category: valid_attributes }
+          }.to change(ServiceCategory, :count).by(1)
+        end
+      end
+
+      context 'with invalid paramters' do
+        before do
+          post :create, params: { service_category: { invalid: true } }
+        end
+
+        it 'returns http error' do
+          expect(response).to have_http_status(400)
+        end
+
+        it 'returns error messages' do
+          expect(body.keys).to eql(['uuid', 'name', 'slug', 'system_code'])
+        end
       end
     end
     
