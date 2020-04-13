fe37152b4de9dd089dd72602219926f355a5d18f
rodrigo@rtoledo.inf.br <rodrigo@rtoledo.inf.br>
2020-04-13 07:40:46 -0300

    fix the ideia when business id isnt or is informed in search action

--- spec/controllers/service_categorie_request_spec.rb
+++ spec/controllers/service_categorie_request_spec.rb
@@ -28,19 +28,53 @@ RSpec.describe ServiceCategoriesController do
 
   describe "Get Service Categories searching by business" do
     let(:body) { JSON.parse(response.body) }
-    before do
-      create_list(:service_category, 10, business_id: 1)
-      create_list(:service_category, 10, business_id: 2)
-      get :filter, params: {business_id: 2}
-    end
-    
-    it "returns http success" do
-      expect(response).to have_http_status(:success)
+    context "when the business is informed" do
+      before do
+        create_list(:service_category, 10, business_id: 1)
+        create_list(:service_category, 10, business_id: 2)
+        create_list(:service_category, 10, business_id: 3)
+        get :filter, params: {business_id: '1|3'}
+      end
+      
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+  
+      it "returns service categories by search business" do
+        expect(body).to have(20).items
+      end
     end
 
-    it "returns service categories by search business" do
-      expect(body).to have(10).items
+    context "when the business is informed but not exists" do
+      before do
+        create_list(:service_category, 10, business_id: 1)
+        get :filter, params: {business_id: '4'}
+      end
+      
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+  
+      it "returns zero service categories by search business" do
+        expect(body).to have(0).items
+      end
     end
+    
+    context "when the business isnt informed" do
+      before do
+        create_list(:service_category, 10, business_id: 1)
+        get :filter
+      end
+      
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+  
+      it "returns zero service categories by search business" do
+        expect(body).to have(0).items
+      end
+    end
+    
   end
   
 end
