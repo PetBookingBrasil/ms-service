642247b4d8c2067e6100fe36ad6ddafafa4ff08b
Jhony Taveira <jhony@jhony-debian.jhony.com>
2020-04-15 16:06:06 -0300

    Config I18n

--- config/application.rb
+++ config/application.rb
@@ -17,17 +17,11 @@ Bundler.require(*Rails.groups)
 
 module MsService
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.available_locales = ['pt-BR', :en]
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.fallbacks = true
+
     config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
