7952187683c6098cad5fe3fb384a8ba7acdf7e71
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2020-04-15 16:34:14 -0300

    simple config with searchkick in the models

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,7 @@ class Service < ApplicationRecord
   belongs_to :service_category
   validates :uuid, :name, :slug, :application, :business_id, presence: true
   validates :uuid, :slug, uniqueness: true
+
+  searchkick word_start: [:name, :slug, :application]
+
 end
--- app/models/service_category.rb
+++ app/models/service_category.rb
@@ -2,4 +2,6 @@ class ServiceCategory < ApplicationRecord
   validates :uuid, :name, :slug, :system_code, presence: true
   validates :uuid, :slug, :system_code, uniqueness: true
   has_ancestry
+
+  searchkick word_start: [:name, :slug, :system_code]
 end
