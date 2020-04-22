3c442019073810e644371dd789d603344f0b0072
Filipe Fenrich <foolemdev@gmail.com>
2020-04-22 13:31:16 -0300

    Scoped by application

--- app/api/v1/service_price_rules.rb
+++ app/api/v1/service_price_rules.rb
@@ -3,9 +3,11 @@ module V1
     include ::V1::Defaults
 
     resource :service_price_rules do
-      desc "Return all service_price_rules"
+      desc "Return all service_price_rules for current application"
       get do
-        present data: V1::Entities::ServicePriceRule.represent(ServicePriceRule.all).as_json
+        present data: V1::Entities::ServicePriceRule.represent(
+          ServicePriceRule.where(application: headers['X-Application'])
+        ).as_json
       end
     end
 
